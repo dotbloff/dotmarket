b864e12d8834614f05a4a5c2a390ece1c6175403
blxssxm <blessed_void@mail.ru>
2024-07-04 15:58:52 +0300

    -

--- cybershop/cybershop/urls.py
+++ cybershop/cybershop/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('shop.urls')),
     path('admin/', admin.site.urls),
-]
+]          
+
+
--- cybershop/shop/models.py
+++ cybershop/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.IntegerField()
+    image_url = models.CharField(max_length=256)
+
